c350ed3d756260e70e4a92d3845f5ae7049f9ec8
Glitch (glitch-hello-website) <none>
2022-11-21 04:04:08 +0000

    👱🏝 Checkpoint ./home/index.php:30526472/375 ./maincodememo.em:30526472/575

--- home/index.php
+++ home/index.php
@@ -7,7 +7,10 @@
   </head>
   <body>
     <h2 class="title">
-      人狼Onlineツールボックスー<?php echo substr($_SERVER["REQUEST_URI"],1,1); ?>
+      人狼Onlineツールボックスー<?php echo substr($_SERVER["REQUEST_URI"],1); ?>
     </h2>
+    <?php
+    echo getJson("https://zinro.net/m/api?mode=rooms");
+    ?>
   </body>
 </html>
--- maincodememo.em
+++ maincodememo.em
@@ -6,6 +6,8 @@
     <title>人狼Onlineツールボックス<?php echo $_SERVER["REQUEST_URI"]; ?></title>
   </head>
   <body>
-    
+    <h2 class="title">
+      人狼Onlineツールボックスー<?php echo substr($_SERVER["REQUEST_URI"],1); ?>
+    </h2>
   </body>
 </html>
