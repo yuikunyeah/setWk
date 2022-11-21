342c0119743e2c009d6284f94dab247cdc755970
Glitch (glitch-hello-website) <none>
2022-11-21 03:52:17 +0000

    🌇🚴 Checkpoint ./index.html:30526472/358 ./tools/index.php:30526472/254 ./home/index.php:30526472/349 ./maincodememo.em:30526472/238

---
+++ home/index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+  <head>
+    <link rel="stylesheet" href="maindesign.css">
+    <script src="mainscript.js"></script>
+    <title>人狼Onlineツールボックス<?php echo $_SERVER["REQUEST_URI"]; ?></title>
+  </head>
+  <body>
+    <h2 class="title">
+      人狼Onlineツールボックスー<?php echo substr($_SERVER["REQUEST_URI"],1,1); ?>
+    </h2>
+  </body>
+</html>
---
+++ maincodememo.em
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+  <head>
+    <link rel="stylesheet" href="maindesign.css">
+    <script src="mainscript.js"></script>
+    <title>人狼Onlineツールボックス<?php echo $_SERVER["REQUEST_URI"]; ?></title>
+  </head>
+  <body>
+    
+  </body>
+</html>
